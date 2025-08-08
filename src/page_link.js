9b2a644860ca832af336ba8d19b53fbc807d2a60
Jaegerwald <86529399+Jaegerwald@users.noreply.github.com>
2025-08-08 14:26:23 +0200

    Added middle click to page links

--- src/page_link.js
+++ src/page_link.js
@@ -2,6 +2,12 @@ class PageLink extends HTMLElement {
     constructor() {
         super();
 
+        this.addEventListener("auxclick", function(e) {
+            if (e.button == 1) {
+                window.open(window.location.origin + "/#" + this.getAttribute("href"), "_blank");
+                return;
+            }
+        });
         this.addEventListener("click", function() {
             window.location.hash = this.getAttribute("href") || "/wiki/Main_page";
             let tabs = document.getElementsByClassName("tab");
